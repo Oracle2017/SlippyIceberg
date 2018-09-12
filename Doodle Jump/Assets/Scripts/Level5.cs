f4b4586793a897ffa73dad83a8c573f45d37c056
tommygraven <tommygraven@me.com>
2018-09-12 20:44:30 +0100

    level 5 start

--- Doodle Jump/Assets/Scripts/Level5.cs
+++ Doodle Jump/Assets/Scripts/Level5.cs
@@ -2,15 +2,47 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Level5 : MonoBehaviour {
+public class Level5 : Level3 {
+	bool isWaiting = true;
 
-	// Use this for initialization
-	void Start () {
-		
+	public override void UpdateSettings () {
+		base.UpdateSettings();
+
+		if (!isPrepared)
+		{
+			return;
+		}
+
+		if (!isWaiting)
+		{
+			StartCoroutine(BlocksFallDownGap(2));
+		}
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	IEnumerator BlocksFallDownGap(float _waitTime)
+	{
+		isWaiting = true;
+
+		InstantiateBlocks();
+		int r = Random.Range(0, blocksFolders.Count);
+
+		GameObject _blocksFolder = blocksFolders[blocksFolders.Count-1];
+
+		for (int i = 0; i < _blocksFolder.transform.childCount; i++)
+		{
+			FallingBlock _block = _blocksFolder.transform.GetChild(i).GetComponent<FallingBlock>();
+			_block.canFall = true;
+		}
+
+		Destroy(_blocksFolder.transform.GetChild(r));
+
+		yield return new WaitForSeconds(_waitTime);
+
+		isWaiting = false;
+	}
+
+	public override void Reset()
+	{
+		base.Reset();
 	}
 }
